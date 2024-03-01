8b0bda7df37bb6a92be70abd4abdda9a6c4b2b60
arzirahman <arzirahman07@gmail.com>
2024-03-01 22:25:39 +0700

    Add Minio

--- Services/MinioService.cs
+++ Services/MinioService.cs
@@ -1,6 +1,5 @@
 using Minio;
 using Minio.DataModel.Args;
-using System.Text.RegularExpressions;
 
 namespace food_order_dotnet.Services
 {
@@ -8,26 +7,6 @@ namespace food_order_dotnet.Services
     {
         private readonly IMinioClient _minioClient = minioClient ?? throw new ArgumentNullException(nameof(minioClient));
         private readonly string? _bucketName = configuration["Minio:BucketName"];
-
-        [GeneratedRegex("[^a-zA-Z0-9]")]
-        private static partial Regex MyRegex();
-        
-        private static string SanitizeForFilename(string input)
-        {
-            return MyRegex().Replace(input, "_");
-        }
-
-        public string GenerateFileName(string baseFileName, string categoryName, string levelName, string extension)
-        {
-            ArgumentNullException.ThrowIfNull(categoryName);
-
-            ArgumentNullException.ThrowIfNull(levelName);
-
-            string timeStamp = DateTime.Now.Ticks.ToString();
-            string fileName = $"{baseFileName}_{SanitizeForFilename(categoryName)}_{SanitizeForFilename(levelName)}_{timeStamp}.{extension}";
-
-            return fileName;
-        }
 
         public async Task<string> GetPresignedUrlAsync(string objectName, int expiryTime)
         {
